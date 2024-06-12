e60ecf81a8a5a259b28786fcf655cd6b656eb51a
junh9541 <74410135+junh9541@users.noreply.github.com>
2024-06-12 23:32:54 +0900

    Title: 변수 출력하기 2, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240612/변수 출력하기 2/outputing-variables-2.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        char c = 'C';
+        int a = 3;
+
+        System.out.printf("%c!.....!%d", c, a);
+    }
+}
