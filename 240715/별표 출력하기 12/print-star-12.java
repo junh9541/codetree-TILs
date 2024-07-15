5728fcbced4b2100011691aa758ce840312a26dd
junh9541 <74410135+junh9541@users.noreply.github.com>
2024-07-15 11:35:53 +0900

    Title: 별표 출력하기 12, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240715/별표 출력하기 12/print-star-12.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        int n= sc.nextInt();
+
+        for (int i=0; i<n; i++){
+            for(int j = 0; j<n; j++){
+                if (i==0)
+                System.out.printf("* ");
+                else if(j%2==1 && j>=i)
+                System.out.printf("* ");
+                else
+                System.out.printf("  ");
+
+            }
+            System.out.println();
+        }
+    }
+}
