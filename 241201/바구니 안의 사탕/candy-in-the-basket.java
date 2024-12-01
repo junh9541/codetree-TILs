0beb317aef57657bdd954f2c3b00673a6df5bfbb
junh9541 <74410135+junh9541@users.noreply.github.com>
2024-12-01 12:03:26 +0900

    Title: 바구니 안의 사탕, Time: 322ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241201/바구니 안의 사탕/candy-in-the-basket.java
+++ 241201/바구니 안의 사탕/candy-in-the-basket.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 import java.io.*;
 
@@ -23,18 +24,27 @@ public class Main {
         	return li1[0]-li2[0];        
         	});
         
+//        System.out.println(Arrays.deepToString(baskets));
+        Deque<int[]> selected = new ArrayDeque<>();
+        selected.add( baskets[0]);
         int start = baskets[0][0];
         int maxCandy=0;
         int candySum = 0;
         
         for(int i = 0; i<N; i++) {
-        	if(baskets[i][0]>start+K*2) {
+        	if(baskets[i][0]>selected.getFirst()[0]+K*2) {
+        		while(!selected.isEmpty()) {
+        			if((baskets[i][0]<=selected.getFirst()[0]+K*2)) {
+        				break;
+        			}
+        			selected.pollFirst();
+        		}
         		if(maxCandy<candySum) {
         			maxCandy=candySum;
         		}
-        		start = baskets[i][0];
         		candySum = 0;
         	}
+        	selected.add(baskets[i]);
     		candySum+=baskets[i][1];
 		}
         if(maxCandy<candySum) {
